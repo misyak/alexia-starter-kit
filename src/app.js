de00f4e9aaa648671aee82e5f25a17568667648f
Michal Morvay <michal.morvay@accenture.com>
2016-12-07 11:23:08 +0100

    test lambCI

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = alexia.createApp();
  * Register callback to be executed once app is started without any intent.
  * Example invocation: 'Alexa, start <my-app-name>'
  */
-app.onStart(() => 'Welcome to Alexia Starter Kit');
+app.onStart(() => 'Welcome to Alexia Starter Kit with LambCI');
 
 // Register all intents matching specified pattern
 app.registerIntents('src/modules/**/*-intent.js');
